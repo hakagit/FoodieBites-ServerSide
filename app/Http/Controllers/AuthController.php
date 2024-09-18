ab2af74163a22165806afc589ec001f33b9ef731
hakagit <mnstrmlk@gmail.com>
2024-09-18 16:01:00 +0300

    fixing login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -31,16 +31,22 @@ class AuthController extends Controller
     // Login user
     public function login(LoginUserRequest $request)
     {
+        // Attempt to authenticate the user with the provided credentials
         if (Auth::attempt($request->only('email', 'password'))) {
-            $user = Auth::user();
-            $token = $user->createToken('foodie-bites')->plainTextToken;
+            $user = Auth::user(); // Get the authenticated user
+            $token = $user->createToken('foodie-bites')->plainTextToken; // Create the token
 
-            return response()->json(['token' => $token]);
+            // Return the token, userId, and userRole in the response
+            return response()->json([
+                'token' => $token,
+                'userId' => $user->id,        // Assuming 'id' is the primary key for your User model
+                'userRole' => $user->role,    // Assuming 'role' is an attribute in your User model
+            ]);
         }
 
+        // Return error response if authentication fails
         return response()->json(['error' => 'Unauthorized'], 401);
     }
-
     // Get authenticated user
     public function showUser(Request $request)
     {
